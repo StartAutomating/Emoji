2e5597f83d71d5403ec77d92f1195daa05f87746
James Brundage <@github.com>
2024-02-21 11:13:31 -0800

    feat: Search/Find-Emoji -Word ( Fixes #16 )

--- Types/Emoji/Search.ps1
+++ Types/Emoji/Search.ps1
@@ -11,17 +11,25 @@ $Pattern,
 
 # If set, will search using the -like operator.  By default, will search using -match
 [switch]
-$Like
+$Like,
+
+# If set, will look for an exact word.  This is not compatible with -Like.
+[switch]
+$Word
 )
 
 if (-not $this) {
     $this = Get-Module Emoji
 }
 
+if ($word) {
+    $like = $false
+    $Pattern = $Pattern -replace '^', '(?<=(?>^|\s))' -replace '$', '(?=(?>$|\s))'
+}
+
 foreach ($condition in $Pattern) {
     foreach ($namedEmoji in $this.Import()) {
-        if ($like) {
-            
+        if ($like) {            
             if ($namedEmoji.Name -like $condition) {
                 $namedEmoji
             }
