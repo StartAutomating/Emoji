f52e8f35384f7d7d13ff59614aabc448bf5995a0
James Brundage <@github.com>
2024-03-03 12:14:01 -0800

    style: Emoji.Symbol.get_PowerShell ( Fixes #49 )

---
+++ Types/Emoji.Symbol/get_PowerShell.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+    Gets an Emoji's PowerShell string
+.DESCRIPTION
+    Gets a PowerShell string that would reproduce the Emoji.
+
+    These strings will only work on PowerShell Core.
+#>
+param()
+'"`u{' + $_.Hex + '}"'
