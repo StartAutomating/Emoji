b53a16a6a0ee592c708601bac81c2b9038d8b078
StartAutomating <StartAutomating@github.com>
2024-03-03 00:18:53 +0000

    feat: Simplifying Set-Emoji ( Fixes #44 )

--- Types/Emoji/Set.ps1
+++ Types/Emoji/Set.ps1
@@ -3,35 +3,14 @@ param(
 [string[]]
 $EmojiSequence,
 
+[Parameter(ValueFromPipelineByPropertyName)]
 [PSObject]
 $Value
 )
 
 $joinedSequence = $EmojiSequence -join ''
 
-$dynamicModule = 
-    if ($joinedSequence -and $value -is [ScriptBlock]) {        
-        New-Module -Name $joinedSequence -ScriptBlock ([ScriptBlock]::Create(
-            "function $JoinedSequence {
-                . `${$JoinedSequence}
-            }
-
-            `${$JoinedSequence} = `$args[0]
-
-            Export-ModuleMember -Function * -Variable * -Alias *"
-        )) -ArgumentList $value    
-    }     
-    else {
-        New-Module -Name $joinedSequence -ScriptBlock ([ScriptBlock]::Create(
-            "`${$JoinedSequence} = `$args[0]
-            Export-ModuleMember -Function * -Variable * -Alias *"
-        )) -ArgumentList $value                
-    }
-
-if ($dynamicModule) {
-    $dynamicModule | Import-Module -Global -Force -DisableNameChecking -PassThru
-}
-
+$emoji.Sequence = $joinedSequence, $value
 
     
 
