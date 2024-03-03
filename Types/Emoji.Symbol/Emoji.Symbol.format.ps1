8ffb8853343a797f5ccb6f526fce04fdcd3b03b3
James Brundage <@github.com>
2024-03-03 12:22:25 -0800

    style: Emoji.Symbol PowerShell format ( Fixes #48 )

--- Types/Emoji.Symbol/Emoji.Symbol.format.ps1
+++ Types/Emoji.Symbol/Emoji.Symbol.format.ps1
@@ -9,6 +9,17 @@ Write-FormatView -TypeName Emoji.Symbol -Property Emoji, Name, Hex -AlignPropert
     Name = 'Foreground.Green'
 }
 
+Write-FormatView -TypeName Emoji.Symbol -Property Emoji, Name, PowerShell -AlignProperty @{
+    Emoji = 'Left'
+    PowerShell   = 'Right'
+    Name  = 'Center'
+} -AutoSize -VirtualProperty @{
+    Emoji = { $_.Emoji.ToString().PadRight(3) }
+} -StyleProperty @{
+    Hex = 'Foreground.Cyan'
+    Name = 'Foreground.Green'
+} -Name PowerShell
+
 Write-FormatView -TypeName Emoji.Symbol -Action {
     Write-FormatViewExpression -ScriptBlock {
         $_.Emoji.PadRight(4)
@@ -16,3 +27,13 @@ Write-FormatView -TypeName Emoji.Symbol -Action {
 
     Write-FormatViewExpression -Property Name -Style Foreground.Green
 }
+
+Write-FormatView -TypeName Emoji.Symbol -Action {
+    Write-FormatViewExpression -ScriptBlock {
+        $_.Emoji.PadRight(4)
+    }
+
+    Write-FormatViewExpression -Property PowerShell -Style Foreground.Cyan
+
+    Write-FormatViewExpression -Property Name -Style Foreground.Green
+} -Name PowerShell
