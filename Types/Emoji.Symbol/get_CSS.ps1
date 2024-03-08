b907b5218b2760675628a431522534230ff82a22
James Brundage <@github.com>
2024-03-08 15:05:26 -0800

    feat: Emoji.Symbol.CSS ( Fixes #58 )

---
+++ Types/Emoji.Symbol/get_CSS.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+    Gets an Emoji as CSS 
+.DESCRIPTION
+    Gets an Emoji as a CSS rule
+#>
+param()
+
+$className = $this.Name.ToLower() -replace '\s','-'
+".$className:before { content: `"$($this.Emoji)`"; }"
