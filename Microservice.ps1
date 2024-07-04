c8f4c3323b8af6f551551645e099960c61bc8f29
James Brundage <@github.com>
2024-07-03 18:34:34 -0700

    feat: Emoji Microservice ( Fixes #69 )

---
+++ Microservice.ps1
@@ -0,0 +1,13 @@
+if (-not $psNode) {
+    $psNode = $emoji.Start()
+    # Output the location of the node being served.
+    @{serving=$($psNode.Location)} | ConvertTo-Json -Compress | Write-Information    
+}
+
+# Wait for the node to finish
+# (If this is running in a headless web server, it should never finish)
+do {
+    Wait-Job -Id $psNode.ID -Timeout ([int](Get-Random -Minimum 1067 -Maximum 2971))
+    # Write the output of the node to the host
+    $psNode | Receive-Job | Out-Host
+} while ($psNode.State -eq 'Running')
