a932fa27f6416c0d8f0ba9996b31e0cacfad34b8
James Brundage <@github.com>
2024-08-05 22:05:45 -0700

    feat: Emoji database ( Fixes #77 )

--- Build/Emoji.Build.ps1
+++ Build/Emoji.Build.ps1
@@ -12,46 +12,41 @@ if (-not $latestUnicodeZip) {
 }
 
 # Put the contents into UCD
-$UCDZipPath = Join-Path $PSScriptRoot "UCD.zip"
-$UCDPath    = Join-Path $PSScriptRoot "UCD"
+$UCDZipPath = Join-Path $pwd "UCD.zip"
+$UCDPath    = Join-Path $pwd "UCD"
 if (-not (Test-path $UCDZipPath)) {
     [IO.File]::WriteAllBytes("$UCDZipPath",$latestUnicodeZip.Content)
     Expand-Archive -Path "$UCDZipPath" -DestinationPath "$UCDPath" -Force
 }
 #endregion Download Unicode Character Dataset
 
-#region Extract, Transform, and Load Named Emoji
-$allNamedEmoji = 
-    Get-ChildItem -Path $UCDPath -Recurse -Filter 'DerivedName.txt' |
-    Import-Csv -Delimiter ';' -Header HexCode, Name
-
-$allUniquelyNamedEmoji = $allNamedEmoji | 
-    Group-Object Name | 
-    Where-Object Count -eq 1
-
 $parentPath = $pwd | Split-Path
 $DataPath = Join-Path $parentPath "Data"
+
+$emojiDataSet = [Data.DataSet]::new('Emoji')
+$blockTable = $emojiDataSet.Tables.Add('Block')
+$blockTable.Columns.AddRange(@(
+    [Data.DataColumn]::new('BlockName', [string], '','Attribute')
+    [Data.DataColumn]::new('RangeStart', [int], '','Attribute')
+    [Data.DataColumn]::new('RangeEnd', [int], '','Attribute')
+))
+$blockTable.PrimaryKey = $blockTable.Columns['BlockName']
+$symbolTable = $emojiDataSet.Tables.Add('Symbol')
+$symbolTable.Columns.AddRange(@(
+    [Data.DataColumn]::new('Number', [int], '','Attribute')
+    [Data.DataColumn]::new('Name', [string], '','Attribute')    
+    [Data.DataColumn]::new('String', [string], '','Attribute')
+    [Data.DataColumn]::new('BlockName', [string], '','Hidden')
+))
+$symbolTable.PrimaryKey = $symbolTable.Columns['Hex']
+$symbolsNestedInBlocks = $emojiDataSet.Relations.Add('Symbol', $emojiDataSet.Tables['Block'].Columns['BlockName'], $symbolTable.Columns['BlockName'])
+$symbolsNestedInBlocks.Nested = $true
+$symbolsNestedInBlocks.ParentKeyConstraint[0].ConstraintName = 'Block'
+
 
 if (-not (Test-Path $DataPath)) {
     New-Item -ItemType Directory -Path $DataPath -Force | Out-Null
 }
-$allNamedEmojiPath = (Join-Path $DataPath AllNamedEmoji.csv)
-@(foreach ($uniquelyNamedEmoji in $allUniquelyNamedEmoji) {
-    $emojiName, $emojiHex = $uniquelyNamedEmoji.Name, "$($uniquelyNamedEmoji.Group[0].HexCode)".Trim()
-    if ($emojiHex -match '\..') { continue }
-    $emojiString = try {
-        $ExecutionContext.InvokeCommand.ExpandString(('`u{',$emojiHex,'}' -join ''))
-    } catch {
-        $null
-    }
-
-    if ($emojiString) {
-        [Ordered]@{Name="$emojiName";Hex="$emojiHex";String="$emojiString"}
-    }
-}) | Export-Csv -Path $allNamedEmojiPath -Force
-
-Get-Item -Path $allNamedEmojiPath
-#endregion Extract, Transform, and Load Named Emoji
 
 #region Extract, Transform, and Load Emoji Blocks
 $allEmojiBlocks = 
@@ -66,6 +61,7 @@ $allEmojiBlocksPath = (Join-Path $DataPath AllEmojiBlocks.csv)
 @(foreach ($emojiBlock in $allEmojiBlocks) {
     
     $rangeStart, $rangeEnd = $emojiBlock.Range -split '\.\.'
+    $null = $blockTable.Rows.Add($emojiBlock.BlockName, "0x$rangeStart" -as [int], "0x$rangeEnd" -as [int])
     
     [PSCustomObject][Ordered]@{
         RangeStart = $rangeStart
@@ -76,7 +72,43 @@ $allEmojiBlocksPath = (Join-Path $DataPath AllEmojiBlocks.csv)
 }) | Export-Csv -Path $allEmojiBlocksPath
 
 Get-Item $allEmojiBlocksPath
-#endregion Extract, Transform, and Load Emoji Categories
+#endregion Extract, Transform, and Load Emoji Blocks
 
+#region Extract, Transform, and Load Named Emoji
+$allNamedEmoji = 
+    Get-ChildItem -Path $UCDPath -Recurse -Filter 'DerivedName.txt' |
+    Import-Csv -Delimiter ';' -Header HexCode, Name
+
+$allUniquelyNamedEmoji = $allNamedEmoji | 
+    Group-Object Name | 
+    Where-Object Count -eq 1
+
+$allNamedEmojiPath = (Join-Path $DataPath AllNamedEmoji.csv)
+@(foreach ($uniquelyNamedEmoji in $allUniquelyNamedEmoji) {
+    $emojiName, $emojiHex = $uniquelyNamedEmoji.Name, "$($uniquelyNamedEmoji.Group[0].HexCode)".Trim()
+    if ($emojiHex -match '\..') { continue }
+    $emojiString = try {
+        $ExecutionContext.InvokeCommand.ExpandString(('`u{',$emojiHex,'}' -join ''))
+    } catch {
+        $null
+    }
+    $emojiNumber = [int]"0x$emojiHex"
+    $blockName = "$($blockTable.Select("$emojiNumber >= RangeStart AND $emojiNumber <= RangeEnd").BlockName)"
+    $null = $symbolTable.Rows.Add($emojiNumber, $emojiName, $emojiString, $blockName)
+
+    if ($emojiString) {
+        [Ordered]@{Name="$emojiName";Hex="$emojiHex";String="$emojiString"}
+    }
+}) | Export-Csv -Path $allNamedEmojiPath -Force
+
+Get-Item -Path $allNamedEmojiPath
+$EmojiXsdPath = Join-Path $DataPath Emoji.xsd
+$emojiDataSet.WriteXmlSchema("$EmojiXsdPath")
+Get-Item -Path $EmojiXsdPath
+$emojiXmlPath = Join-Path $DataPath Emoji.xml
+$emojiDataSet.WriteXml("$emojiXmlPath")
+Get-Item -Path $emojiXmlPath
+
+#endregion Extract, Transform, and Load Named Emoji
 
 Pop-Location
