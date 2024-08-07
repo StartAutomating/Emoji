5aff87245f0cc26d151e36f2a85d5c92fc1664d7
James Brundage <@github.com>
2024-08-06 19:49:56 -0700

    fix: Emoji.Import now calls Emoji.DB ( Fixes #77, Fixes #81 )

--- Types/Emoji/Import.ps1
+++ Types/Emoji/Import.ps1
@@ -21,23 +21,6 @@ if (-not $this) {
     $this = Get-Module Emoji
 }
 if ((-not $this.'.EmojiCache') -or $Force) {
-    if (-not $DataPath) {
-        $DataPath = $this | 
-            Split-Path | 
-            Join-Path -ChildPath Data
-    }
-    $emojiXsd = Join-Path $DataPath "Emoji.xsd"
-    $emojiXml = Join-Path $DataPath "Emoji.xml"
-    $emojiDataSet = [Data.DataSet]::new('Emoji')
-    $null = $emojiDataSet.ReadXmlSchema($emojiXsd)
-    $null = $emojiDataSet.ReadXml($emojiXml)        
-    Add-Member -InputObject $this -MemberType NoteProperty -Force -Name ".EmojiCache" -Value @(foreach ($importedEmoji in $emojiDataSet.Tables['Symbol'].Rows) {
-        $importedEmoji.pstypenames.insert(0,'Emoji.Symbol')
-        $importedEmoji
-    })
-    Add-Member -InputObject $this -MemberType NoteProperty -Force -Name ".Blocks" -Value @(foreach ($importedBlock in $emojiDataSet.Tables['Block'].Rows) {
-        $importedBlock.pstypenames.insert(0,'Emoji.Block')
-        $importedBlock
-    })
+    Add-Member -InputObject $this -MemberType NoteProperty -Force -Name ".EmojiCache" -Value $this.DB.Tables['Symbol']    
 }
 $this.'.EmojiCache'
