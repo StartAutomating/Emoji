4ab9c0611704179d8338c2a42e91da8d95b586e3
James Brundage <@github.com>
2024-08-06 19:48:00 -0700

    fix: Emoji.get_Blocks now returns the Block table ( Fixes #77 )

--- Types/Emoji/get_Blocks.ps1
+++ Types/Emoji/get_Blocks.ps1
@@ -1,12 +1,5 @@
-if (-not $this.'.Blocks') {
-    $theseBlocks = [Ordered]@{}
-    $allEmojiBlocks = Get-Module Emoji | Split-Path | Join-Path -ChildPath "Data" | Join-Path -ChildPath "AllEmojiBlocks.csv" | Import-Csv
-    foreach ($emojiBlock in $allEmojiBlocks) {
-        $emojiBlock.pstypenames.clear()
-        $emojiBlock.pstypenames.add('Emoji.Block')
-        $theseBlocks[$emojiBlock.BlockName] = $emojiBlock
-    }
-    Add-Member -InputObject $this -MemberType NoteProperty -Force -Name ".Blocks" -Value $theseBlocks
+if (-not $this.'.Blocks') {        
+    Add-Member -InputObject $this -MemberType NoteProperty -Force -Name ".Blocks" -Value $this.DB.Block
 }
 
 $this.'.Blocks'
